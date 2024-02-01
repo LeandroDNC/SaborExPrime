7fd92ec908aefeba4c8845efe7958ee479b49a60
LeandroDNC <leadnromarquesdeusefiel@gmail.com>
2024-02-01 19:31:04 -0300

    Atualização

---
+++ index.js
@@ -0,0 +1,9 @@
+function verSenha() {
+    var senha = document.getElementById("password")
+    if (senha.type === "password") {
+        senha.type = "text!"
+    } else {
+        senha.type = "password"
+    }
+
+}
